8f4e3ea89f920c9206c45462f233c46d7a0b5d86
Aidan Gomez <agomez@venturemedia.com>
2015-07-13 10:44:22 -0700

    Modify FFT method, Move from DataTransformer to Indiv. layers

--- src/caffe/data_transformer.cpp
+++ src/caffe/data_transformer.cpp
@@ -7,7 +7,6 @@
 #include "caffe/util/io.hpp"
 #include "caffe/util/math_functions.hpp"
 #include "caffe/util/rng.hpp"
-#include "caffe/util/fft.hpp"
 
 namespace caffe {
 
@@ -51,7 +50,6 @@ void DataTransformer<Dtype>::Transform(const Datum& datum,
   const bool has_mean_file = param_.has_mean_file();
   const bool has_uint8 = data.size() > 0;
   const bool has_mean_values = mean_values_.size() > 0;
-  const bool has_fft = param_.fft();
 
   CHECK_GT(datum_channels, 0);
   CHECK_GE(datum_height, crop_size);
@@ -73,12 +71,6 @@ void DataTransformer<Dtype>::Transform(const Datum& datum,
         mean_values_.push_back(mean_values_[0]);
       }
     }
-  }
-
-  auto floatData = datum.float_data();
-  if (has_fft) {
-    caffe::FastFourierTransform fft(floatData.size());
-    fft.process(floatData.begin(), floatData.size());
   }
 
   int height = datum_height;
@@ -110,9 +102,7 @@ void DataTransformer<Dtype>::Transform(const Datum& datum,
         } else {
           top_index = (c * height + h) * width + w;
         }
-        if (has_fft) {
-          datum_element = floatData.Get(data_index);
-        } else if (has_uint8) {
+        if (has_uint8) {
           datum_element =
             static_cast<Dtype>(static_cast<uint8_t>(data[data_index]));
         } else {
