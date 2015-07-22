1e2ad5aa93c858afe9c14e6c343986f4fed58faa
Alejandro Isaza <aisaza@venturemedia.com>
2015-07-22 15:14:39 -0700

    Add Framework target

---
+++ Caffe/Caffe.h
@@ -0,0 +1,19 @@
+//
+//  Caffe.h
+//  Caffe
+//
+//  Created by Alejandro Isaza on 2015-07-22.
+//  Copyright (c) 2015 Venture Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Caffe.
+FOUNDATION_EXPORT double CaffeVersionNumber;
+
+//! Project version string for Caffe.
+FOUNDATION_EXPORT const unsigned char CaffeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Caffe/PublicHeader.h>
+
+
