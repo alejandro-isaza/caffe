70704bb30567eb73400032640dec719f1bece824
Aidan Gomez <agomez@venturemedia.com>
2015-08-17 14:02:06 -0700

    Add two new kernel constructors

--- include/caffe/util/assign_conv_weights.h
+++ include/caffe/util/assign_conv_weights.h
@@ -3,19 +3,26 @@
 
 
 #pragma once
-#include <vector>
+#include <valarray>
 #include <memory>
 
 #include "caffe/solver.hpp"
-
-
+#include "caffe/util/fft.hpp"
+#include "caffe/util/read_audio.hpp"
 
 namespace caffe {
-    
+
 template <typename T>
 class KernelGen {
 public:
     KernelGen(int numKernels, int kernelSize) : kNumKernels(numKernels), kKernelSize(kernelSize){}
+    KernelGen(KernelGen& other) : kNumKernels(other.kNumKernels), kKernelSize(other.kKernelSize){}
+    virtual ~KernelGen(){}
+    virtual KernelGen& operator=(const KernelGen& rhs) {
+        kNumKernels = rhs.kNumKernels;
+        kKernelSize = rhs.kKernelSize;
+        return *this;
+    }
     virtual inline void generateKernel(T* data) {
         auto shape = {kNumKernels, 1, 1, kKernelSize};
         auto blob = std::make_shared<caffe::Blob<T>>(shape);
@@ -26,25 +33,31 @@ public:
     static inline T curve(T x, T a, T b) { return std::exp((x-a)*(x-a)/b); }
 
 protected:
-    virtual inline std::vector<T> kernelGen() { return {}; };
+    virtual std::valarray<T> kernelGen() = 0;
 
 protected:
-    const int kNumKernels;
-    const int kKernelSize;
+    int kNumKernels;
+    int kKernelSize;
 };
 
 template <typename T>
 class ThickKernel : public KernelGen<T> {
 public:
     ThickKernel(int numKernels, int kernelSize) : KernelGen<T>(numKernels, kernelSize) {}
+    ThickKernel(ThickKernel& other) : KernelGen<T>(other) {}
+    ~ThickKernel(){}
+    ThickKernel operator=(ThickKernel rhs) {
+        KernelGen<T>::operator=(rhs);
+        return *this;
+    }
 
 protected:
-    inline std::vector<T> kernelGen() {
+    inline std::valarray<T> kernelGen() {
         const auto numKernels = KernelGen<T>::kNumKernels;
         const auto kernelSize = KernelGen<T>::kKernelSize;
 
         auto step = static_cast<double>(kernelSize) / numKernels;
-        auto kernelWeights = std::vector<T>(kernelSize * numKernels);
+        auto kernelWeights = std::valarray<T>(kernelSize * numKernels);
 
         for (auto kernel = 0; kernel < numKernels; ++kernel) {
             auto peakLocation = step * kernel / kernelSize;
@@ -189,6 +202,72 @@ protected:
 };
 
 template <typename T>
+class ThinKernel : public KernelGen<T> {
+public:
+    ThinKernel(int numKernels, int kernelSize) : KernelGen<T>(numKernels, kernelSize) {}
+    ThinKernel(ThinKernel& other) : KernelGen<T>(other) {}
+    ~ThinKernel(){}
+
+protected:
+    inline std::valarray<T> kernelGen() {
+        const auto numKernels = KernelGen<T>::kNumKernels;
+        const auto kernelSize = KernelGen<T>::kKernelSize;
+
+        auto step = static_cast<double>(kernelSize) / numKernels;
+        auto kernelWeights = std::valarray<T>(kernelSize * numKernels);
+
+        for (auto kernel = 0; kernel < numKernels; ++kernel) {
+            auto peakLocation = step * kernel / kernelSize;
+            for (auto position = 0; position < kernelSize; ++position) {
+                auto x = static_cast<double>(position) / kernelSize;
+                kernelWeights[(kernel * kernelSize) + position] = KernelGen<T>::curve(x, peakLocation, kB);
+            }
+        }
+
+        return kernelWeights;
+    }
+
+protected:
+    double kB = -0.0005;
+};
+
+template <typename T>
+class FFTKernel : public KernelGen<T> {
+public:
+    FFTKernel(int numKernels, int kernelSize) : KernelGen<T>(numKernels, kernelSize) {}
+    FFTKernel(FFTKernel& other) : KernelGen<T>(other) {}
+    ~FFTKernel(){}
+
+protected:
+    inline std::valarray<T> kernelGen() {
+        const auto numKernels = KernelGen<T>::kNumKernels;
+        const auto kernelSize = KernelGen<T>::kKernelSize;
+
+        auto data = std::valarray<T>(numKernels * kernelSize);
+        auto fftBuffer = std::valarray<T>(2 * kernelSize);
+
+        const auto options = caffe::FFTOptions{};
+
+        for (auto i = 0; i < numKernels; ++i) {
+            if (i < 84) {
+                const auto filename = "data/Training/Notes/AcousticGrandPiano_YDP/" + std::to_string(i + 24) + ".caf";
+                caffe::ReadAudioFile(filename, std::begin(fftBuffer), 2 * kernelSize);
+
+                auto fft = caffe::FastFourierTransform<T>(2 * kernelSize, options);
+                fft.process(std::begin(fftBuffer), 2 * kernelSize);
+
+                 std::move(std::begin(fftBuffer), std::begin(fftBuffer) + kernelSize, std::begin(data) + (i * kernelSize));
+            } else {
+                auto dataStart = std::begin(data) + ((i % 84) * kernelSize);
+                std::move_backward(dataStart, dataStart + kernelSize, std::begin(data) + (i * kernelSize));
+            }
+        }
+
+        return data;
+    }
+};
+
+template <typename T>
 std::shared_ptr<caffe::Blob<T>> generateKernel(int numKernels, int kernelSize);
 template <typename T>
 void assignConvolutionWeights(std::shared_ptr<caffe::Net<T>> net);
@@ -202,7 +281,7 @@ inline void initializeSolverWeights(caffe::Solver<T> solver) {
 }
 
 template <typename T>
-inline void assignConvolutionWeights(std::shared_ptr<caffe::Net<T>> net) {
+    inline void assignConvolutionWeights(std::shared_ptr<caffe::Net<T>> net, caffe::FillerParameter_PeakType kernelType) {
     for (auto layer : net->layers()) {
         if (layer->type() == "Convolution") {
             auto parameters = layer->layer_param().convolution_param();
@@ -225,7 +304,7 @@ inline void assignConvolutionWeights(std::shared_ptr<caffe::Net<T>> net) {
             }
 
             auto data = static_cast<T*>(layer->blobs()[0]->data()->mutable_cpu_data()); // first blob is weights, second is biases
-            kernel.generateKernel(data);
+            kernel->generateKernel(data);
             break;
         }
     }
