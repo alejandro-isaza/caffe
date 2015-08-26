bf95223313742d995174cd42ee160302bfb36bb8
Aidan Gomez <agomez@venturemedia.com>
2015-08-26 09:48:36 -0700

    Fix file closing in audio reading

--- src/caffe/util/read_audio_unix.cpp
+++ src/caffe/util/read_audio_unix.cpp
@@ -7,35 +7,42 @@
 #include <sndfile.h>
 
 namespace caffe {
-    
+
     int ReadAudioFile(const std::string& filePath, float* data, int capacity, int offset) {
         auto info = SF_INFO{};
-        
+
+        auto file = sf_open(filePath.c_str(), SFM_READ, &info);
+
+        CHECK_EQ(sf_error(file), SF_ERR_NO_ERROR) << "Can't open file '" << filePath << "': " << sf_strerror(file);
+
+        auto status = sf_seek(file, offset, SEEK_SET);
+        CHECK_NE(status, -1) << "Can't seek to offset in: '" << filePath << "': " << sf_strerror(file);
+
+        auto numberOfFrames = sf_read_float(file, data, capacity);
+        CHECK_EQ(numberOfFrames, capacity) << "File could not fill provided array";
+
+        status = sf_close(file);
+        CHECK_EQ(status, 0) << "Failed to close file: ''" << filePath << "': " << sf_strerror(file);
+
+        return numberOfFrames;
+    }
+
+    int ReadAudioFile(const std::string& filePath, double* data, int capacity, int offset) {
+        auto info = SF_INFO{};
+
         auto file = sf_open(filePath.c_str(), SFM_READ, &info);
         CHECK_EQ(sf_error(file), SF_ERR_NO_ERROR) << "Can't open file '" << filePath << "': " << sf_strerror(file);
 
         auto status = sf_seek(file, offset, SEEK_SET);
         CHECK_NE(status, -1) << "Can't seek to offset in: '" << filePath << "': " << sf_strerror(file);
-        
-        auto numberOfFrames = sf_read_float(file, data, capacity);
-        CHECK_EQ(numberOfFrames, capacity) << "File could not fill provided array";
-        
-        return numberOfFrames;
-    }
-    
-    int ReadAudioFile(const std::string& filePath, double* data, int capacity, int offset) {
-        auto info = SF_INFO{};
-        
-        auto file = sf_open(filePath.c_str(), SFM_READ, &info);
-        CHECK_EQ(sf_error(file), SF_ERR_NO_ERROR) << "Can't open file '" << filePath << "': " << sf_strerror(file);
-        
-        auto status = sf_seek(file, offset, SEEK_SET);
-        CHECK_NE(status, -1) << "Can't seek to offset in: '" << filePath << "': " << sf_strerror(file);
-        
+
         auto numberOfFrames = sf_read_double(file, data, capacity);
         CHECK_EQ(numberOfFrames, capacity) << "File could not fill provided array";
-        
+
+        status = sf_close(file);
+        CHECK_EQ(status, 0) << "Failed to close file: ''" << filePath << "': " << sf_strerror(file);
+
         return numberOfFrames;
     }
-    
+
 } // namespace caffe
